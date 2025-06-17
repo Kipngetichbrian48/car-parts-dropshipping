20c1262a9d3b3fff78c1ae84f2ebb91ffa45de6a
Kipngetichbrian48 <brotich438@gmail.com>
2025-06-17 15:38:28 +0300

    Fixed by setting EJS as the view engine

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const app = express();
 
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public'));
 
 app.use(express.static('public'));
